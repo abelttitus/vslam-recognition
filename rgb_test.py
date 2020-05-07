078039b4c45684ba9ab371a5956a1e099737442d
abelttitus <abelttitus1998@gmail.com>
2020-05-07 11:43:34 +0530

    rgb-test-mod

--- rgb_test.py
+++ rgb_test.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 if __name__ =='__main__':
-   rgb_img=cv2.imread('/home/abel/dataset/rgb/1.png') 
+   rgb_img=cv2.imread('/home/abel/rgbd_dataset_freiburg1_xyz/rgb/1305031110.879313.png') 
    print("Max Value:",np.max(rgb_img))
    print("Min Value:",np.min(rgb_img))
    print("Shape:",rgb_img.shape)
