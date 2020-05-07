5744d9e3eaa8c54c640e9ac9108826e295d8ba8f
abelttitus <abelttitus1998@gmail.com>
2020-05-07 15:32:10 +0530

    pcd-test-debug-3

--- pcd.py
+++ pcd.py
@@ -33,28 +33,21 @@ if __name__=='__main__':
     img=cv2.imread(img_path)
     depth=cv2.imread(depth_path)
     
-    cv2.imshow('Color',img)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    cv2.imshow('Depth',depth)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-# =============================================================================
-#     fx = 525.0  # focal length x
-#     fy = 525.0  # focal length y
-#     cx = 319.5  # optical center x
-#     cy = 239.5  # optical center y
-#     
-#     factor = 5000.0 # for the 16-bit PNG files
-#     
-#     coords=np.zeros((480,640,3))
-#     coords.astype(np.float32)
-#     for y in range(480):
-#         for x in range(640):
-#             coords[y,x,2]=float(depth[y,x,0])/factor
-#             Z=coords[y,x,2]
-#             coords[y,x,1]=(x-cx)*Z/fx
-#             coords[y,x,0]=(y-cy)*Z/fy
-#     print(coords)      
-# =============================================================================
-            
+
+    fx = 525.0  # focal length x
+    fy = 525.0  # focal length y
+    cx = 319.5  # optical center x
+    cy = 239.5  # optical center y
+    
+    factor = 5000.0 # for the 16-bit PNG files
+    
+    coords=np.zeros((480,640,3))
+    coords.astype(np.float32)
+    for y in range(480):
+        for x in range(640):
+            coords[y,x,2]=float(depth[y,x,0])/factor
+            Z=coords[y,x,2]
+            coords[y,x,1]=(x-cx)*Z/fx
+            coords[y,x,0]=(y-cy)*Z/fy
+    print(np.unique(coords))      
+    print(np.unique(depth))
