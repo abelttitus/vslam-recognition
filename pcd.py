a353eeba21307b8e7b777381ae270de3b3acbbf9
abelttitus <abelttitus1998@gmail.com>
2020-05-07 15:22:27 +0530

    pcd-test-float

--- pcd.py
+++ pcd.py
@@ -9,7 +9,7 @@ Created on Thu May  7 12:15:54 2020
 
 import numpy as np
 import cv2
-import pickle
+import open3d as o3d
 
 if __name__=='__main__':
     verbose=True;
@@ -38,18 +38,15 @@ if __name__=='__main__':
     cx = 319.5  # optical center x
     cy = 239.5  # optical center y
     
-    factor = 5000 # for the 16-bit PNG files
+    factor = 5000.0 # for the 16-bit PNG files
     
     coords=np.zeros((480,640,3))
+    coords.astype(np.float32)
     for y in range(480):
         for x in range(640):
-            coords[y,x,2]=depth[y,x,0]/factor
+            coords[y,x,2]=float(depth[y,x,0])/factor
             Z=coords[y,x,2]
             coords[y,x,1]=(x-cx)*Z/fx
             coords[y,x,0]=(y-cy)*Z/fy
-    db_file=open('coords','ab')
-    pickle.dump(coords,db_file)
-    db_file.close()
-            
-            
+    print(coords)      
             
