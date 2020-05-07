455823385d537419d0fb115417a34dce9f935be2
abelttitus <abelttitus1998@gmail.com>
2020-05-07 13:01:55 +0530

    pcd-test-3

--- pcd.py
+++ pcd.py
@@ -42,9 +42,9 @@ if __name__=='__main__':
     coords=np.zeros((480,640,3))
     for y in range(480):
         for x in range(640):
-            Z=depth[y,x]/factor
-            X=(x-cx)*Z/fx
-            Y=(y-cy)*Z/fy
-            coords[y,x,:]=np.array([X,Y,Z])
+            coords[y,x,2]=Z=depth[y,x]/factor
+            coords[y,x,1]=(x-cx)*Z/fx
+            coords[y,x,0]=(y-cy)*Z/fy
+            
             
             
