ce49aed680cfe65ae7fe53a98a99c84d7f9609da
abelttitus <abelttitus1998@gmail.com>
2020-05-16 10:39:33 +0530

    count-4

--- pcd_i_gpu.py
+++ pcd_i_gpu.py
@@ -45,8 +45,15 @@ def generate_pointcloud(rgb_file,depth_file,ply_file):
     print(np.min(rgb))
     depth = cv2.imread(depth_file)
     depth=depth.astype(np.float32)
-    points=np.zeros((depth.shape[0],depth.shape[1],3))
-    points=points.astype(np.float32)
+    
+    #coords
+    x=np.zeros((depth.shape[0],depth.shape[1],3))
+    x=x.astype(np.float32)
+    y=np.zeros((depth.shape[0],depth.shape[1],3))
+    y=y.astype(np.float32)
+    z=np.zeros((depth.shape[0],depth.shape[1],3))
+    z=z.astype(np.float32)
+    
     
     if rgb.shape != depth.shape:
         raise Exception("Color and depth image do not have the same resolution.")
@@ -56,50 +63,47 @@ def generate_pointcloud(rgb_file,depth_file,ply_file):
     #     raise Exception("Depth image is not in intensity format")
 
     depth_gpu=gpuarray.to_gpu(depth)
-    count=np.zeros((480,640,1),dtype=np.float32)
-    points_gpu=cuda.mem_alloc(points.nbytes)
-    count_gpu=cuda.mem_alloc(count.nbytes)
+    x_gpu=cuda.mem_alloc(x.nbytes)
+    y_gpu=cuda.mem_alloc(y.nbytes)
+    z_gpu=cuda.mem_alloc(z.nbytes)    
     
-    cuda.memcpy_htod(points_gpu,points)
-    cuda.memcpy_htod(count_gpu,count)
+    cuda.memcpy_htod(x_gpu,x)
+    cuda.memcpy_htod(y_gpu,y)
+    cuda.memcpy_htod(z_gpu,z)
+    
     
     
     mod=SourceModule("""
-                     __global__ void vmap_kernel(float* depth,float* points,float* count){
+                     __global__ void vmap_kernel(float* depth,float *x,float* y,float* z){
                        int u = threadIdx.x + blockIdx.x * blockDim.x;
                        int v = threadIdx.y + blockIdx.y * blockDim.y; 
                        
                        int cols=640;
                        int rows=480;
                        float scaling_factor=5000.0;
-                       float depthCutoff=20.0f;
                        float cx=319.50;
                        float cy=239.50;
                        float fx_inv=1/481.20;
                        float fy_inv=-1/480.00;
                        
                        if(u<cols && v<rows){
-                               count[u+cols*v]=1.0;
-                               float z= depth[u+cols*v]/scaling_factor;
-                               if(z!=0 && z<depthCutoff){
-                                       float vx = z * (u - cx) * fx_inv;
-                                        float vy = z * (v - cy) * fy_inv;
-                                        float vz = z;
-                                       
-                                        points[u+cols*v]=1.0;
-                                        points[u+cols*v+1]=2.0;
-                                        points[u+cols*v+2]=3.0;
-                                        
-                                        
+                               
+                               float d= depth[u+cols*v]/scaling_factor;
+                               if(d!=0){
+                                       x[u+cols*v] =1.0;
+                                       y[u+cols*v]= 2.0;
+                                       z[u+cols*v]= 3.0;
                                }
                        }
                        }""")
                 
     function=mod.get_function("vmap_kernel");
-    function(depth_gpu,points_gpu,count_gpu,block=(32,8,1),grid=(20,60,1))
-    cuda.memcpy_dtoh(points,points_gpu)
-    cuda.memcpy_dtoh(count,count_gpu)
+    function(depth_gpu,x_gpu,y_gpu,z_gpu,block=(32,8,1),grid=(20,60,1))
+    cuda.memcpy_dtoh(x,x_gpu)
+    cuda.memcpy_dtoh(y,y_gpu)
+    cuda.memcpy_dtoh(z,z_gpu)
     
+    points=np.concatenate([x,y,z],axis=2)
     print("Random values in points along z axis",points[40,50,:],points[32,90,:])
     pcd_color=np.concatenate([points,rgb])
     pcd_color=pcd_color.reshape((640*480,6))
